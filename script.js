ebf818d3501b9384599fec8143b4db020f698741
m-hansson <m.hansson@accenture.com>
2017-01-24 13:25:22 +0100

    Some cleanup of code.

--- script.js
+++ script.js
@@ -3,31 +3,46 @@ $(document).ready(function() {
 	var _URLbase = "https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&format=json&search=";
 	var searchInput;
 	var searchSuggestions;
-	var submitButton;
+
 	
 	var initiate = function() {
 		
         searchInput = $('[name=search-term]');
 		searchSuggestions = $('.search-suggestions');
-		submitButton = $('#search-form [type=submit]');
+		var submitButton = $('#search-form [type=submit]');
+		var results
 		
         $('#search-form').submit(doSearch);
         searchInput.keyup(showSuggestions);
+		
 		$('#search-form .search-icon').click( function() {
 			submitButton.click();
 		});
+		
 		$(document).keypress(function(e) {
 			if (e.which == 13) {
 				submitButton.click();
 			};
 		});
 		
-		
-		//Hide search suggestions after input loses focus. Slight delay so that suggestions are clickable.
+		//Hide search suggestions after input loses focus. Slight delay so that suggestions are clickable
 		searchInput.blur(function() { setTimeout(function(){ 
 			searchSuggestions.addClass('hidden');
 		}, 100); });
 
+		$(".results-list").on('DOMNodeInserted', function(e) {
+			if($(this).children().length > 0) {
+				$(this).removeClass('hidden');
+			};
+		});
+
+		$(".results-list").on('DOMNodeRemoved', function(e) {
+			//Node is removed after this callback therefor length 2
+			if($(this).children().length < 2) {
+				$(this).addClass('hidden');
+			};
+		});
+		
     };
 	
 	var doSearch = function(e) {
@@ -37,6 +52,8 @@ $(document).ready(function() {
 		
 		var searchTerm = searchInput.val();
 		var fullSearch = _URLbase + searchTerm;
+		
+		//TODO: handle malicious input
 		
 		if (searchTerm == "") {
 			return;
@@ -48,7 +65,10 @@ $(document).ready(function() {
 			if (searchResult.length > 0) {
 				
 				//Print item and add remove functionality to button
-				var newItem = "<li class='result-item'>" + "<span class='result-term'>" + searchResult[0] + "</span>" + "<span class='timestamp'>" + timestamp + "</span>" + "<span class='icon-font remove' title='Delete search'></span></li>";
+				var newItem = "<li class='result-item'>" +
+								"<span class='result-term'>" + searchResult[0] + "</span>" + 
+								"<span class='timestamp'>" + timestamp + "</span>" + 
+								"<span class='icon-font remove' title='Delete search'></span></li>";
 				$(".results-list").prepend(newItem);
 				$(".results-list").find('.remove').first().click(function() {
 					$(this).parent().remove();
