a6324e258907724fc2d2e717a8afbf0498190dc2
m-hansson <m.hansson@accenture.com>
2017-01-24 11:45:38 +0100

    Styling and functionality finished.

--- script.js
+++ script.js
@@ -2,58 +2,94 @@ $(document).ready(function() {
 	
 	var _URLbase = "https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&format=json&search=";
 	var searchInput;
+	var searchSuggestions;
+	var submitButton;
 	
 	var initiate = function() {
 		
         searchInput = $('[name=search-term]');
+		searchSuggestions = $('.search-suggestions');
+		submitButton = $('#search-form [type=submit]');
 		
         $('#search-form').submit(doSearch);
         searchInput.keyup(showSuggestions);
-		$('.search-suggestions').focusout(function() {$(this).addClass('hidden')});
+		$('#search-form .search-icon').click( function() {
+			submitButton.click();
+		});
+		$(document).keypress(function(e) {
+			if (e.which == 13) {
+				submitButton.click();
+			};
+		});
+		
+		
+		//Hide search suggestions after input loses focus. Slight delay so that suggestions are clickable.
+		searchInput.blur(function() { setTimeout(function(){ 
+			searchSuggestions.addClass('hidden');
+		}, 100); });
+
     };
 	
 	var doSearch = function(e) {
 		
 		e.preventDefault();
+		searchSuggestions.addClass('hidden');
 		
-		var term = searchInput.val();
 		var searchTerm = searchInput.val();
 		var fullSearch = _URLbase + searchTerm;
 		
+		if (searchTerm == "") {
+			return;
+		};
+		
 		$.get(fullSearch, function(responseData) {
 			var searchResult = cleanResponse(responseData);
-			
+			var timestamp = getTimestamp();
 			if (searchResult.length > 0) {
-				$(".results-list").prepend("<li class='result-item'>" + searchResult[0] + "</li>");
+				
+				//Print item and add remove functionality to button
+				var newItem = "<li class='result-item'>" + "<span class='result-term'>" + searchResult[0] + "</span>" + "<span class='timestamp'>" + timestamp + "</span>" + "<span class='icon-font remove' title='Delete search'></span></li>";
+				$(".results-list").prepend(newItem);
+				$(".results-list").find('.remove').first().click(function() {
+					$(this).parent().remove();
+				});
+				
 			};
-			searchInput.val('');
 			
 		}, 'json');
+		
+		searchInput.val('');
 		
 	};
 	
 	var showSuggestions = function() {
 		
-		var fullSearch = _URLbase + searchInput.val();
+		var searchTerm = searchInput.val();
+		if (searchTerm == "") {
+			return;
+		};
+		
+		var fullSearch = _URLbase + searchTerm;
 		
 		$.get(fullSearch, function(responseData) {
 			var searchResult = cleanResponse(responseData);
 			var suggestionsList = $('.suggestions-list');
 			
-			$('.search-suggestions').removeClass('hidden');
+			searchSuggestions.removeClass('hidden');
 			suggestionsList.html('');
 			
 			if(searchResult.length > 0)	{
 				
-				//Print each result term and make them on click populate the input field.
+				//Print each result term, make them populate the input field on click
 				$.each(searchResult, function(i, term) {
 					suggestionsList.append("<li class='suggestions-item'>" + term + "</li>");
 				});
 				
 				suggestionsList.children().click(function() {
 					searchInput.val($(this).text());
-					$('.search-suggestions').addClass('hidden');
+					searchSuggestions.addClass('hidden');
 				});
+				
 			}
 			
 			else {
@@ -61,6 +97,17 @@ $(document).ready(function() {
 			};
 			
 		}, 'json');
+	};
+	
+	var getTimestamp = function() {
+		var now = new Date();
+		var month = now.getMonth() + 1;
+		var minute = now.getMinutes();
+		if (minute < 10) {
+			minute = '0' + minute;
+		};
+		var timestamp = now.getFullYear() + '-' + month + '-' + now.getDate() + ' ' + now.getHours() + ':' + minute;
+		return timestamp;
 	};
 	
 	var cleanResponse = function(responseData) {
