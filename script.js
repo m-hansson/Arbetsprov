bce103d705600165a321a45a7628938cc6055aec
m-hansson <m.hansson@accenture.com>
2017-01-25 15:26:06 +0100

    Bug fix and minor code cleanup.

--- script.js
+++ script.js
@@ -22,28 +22,28 @@ $(document).ready(function() {
 		$(document).keypress(function(e) {
 			if (e.which == 13) {
 				submitButton.click();
-			};
+			}
 		});
 		
 		//Hide search suggestions after input loses focus. Slight delay so that suggestions are clickable
 		searchInput.blur(function() { setTimeout(function(){ 
 			searchSuggestions.addClass('hidden');
-		}, 100); });
+		}, 200); });
 
 		$(".results-list").on('DOMNodeInserted', function(e) {
 			if($(this).children().length > 0) {
 				$(this).removeClass('hidden');
-			};
+			}
 		});
 
 		$(".results-list").on('DOMNodeRemoved', function(e) {
 			//Node is removed after this callback therefor length 2
 			if($(this).children().length < 2) {
 				$(this).addClass('hidden');
-			};
+			}
 		});
 		
-    };
+    }
 	
 	var doSearch = function(e) {
 		
@@ -57,7 +57,7 @@ $(document).ready(function() {
 		
 		if (searchTerm == "") {
 			return;
-		};
+		}
 		
 		$.get(fullSearch, function(responseData) {
 			var searchResult = cleanResponse(responseData);
@@ -74,20 +74,20 @@ $(document).ready(function() {
 					$(this).parent().remove();
 				});
 				
-			};
+			}
 			
 		}, 'json');
 		
 		searchInput.val('');
 		
-	};
+	}
 	
 	var showSuggestions = function() {
 		
 		var searchTerm = searchInput.val();
 		if (searchTerm == "") {
 			return;
-		};
+		}
 		
 		var fullSearch = _URLbase + searchTerm;
 		
@@ -114,10 +114,10 @@ $(document).ready(function() {
 			
 			else {
 				suggestionsList.append("<li class='suggestions-item'>No matching articles</li>");
-			};
+			}
 			
 		}, 'json');
-	};
+	}
 	
 	var getTimestamp = function() {
 		var now = new Date();
@@ -125,15 +125,15 @@ $(document).ready(function() {
 		var minute = now.getMinutes();
 		if (minute < 10) {
 			minute = '0' + minute;
-		};
+		}
 		var timestamp = now.getFullYear() + '-' + month + '-' + now.getDate() + ' ' + now.getHours() + ':' + minute;
 		return timestamp;
-	};
+	}
 	
 	var cleanResponse = function(responseData) {
 		var resultsArray = responseData[1];
 		return resultsArray;
-	};
+	}
 	
 	initiate();
 	
